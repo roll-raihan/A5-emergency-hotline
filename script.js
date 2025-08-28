3513056b4de23a42992211551e0cc7149f45da26
roll-raihan <raihanahmednasim@gmail.com>
2025-08-28 15:51:51 +0600

    functionality of first card for call and heart icon

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('yuo')
+let heartCount = 0;
+document.getElementById('heart-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    heartCount++;
+    const countHeartValue = document.getElementById('heart-number');
+    countHeartValue.textContent = heartCount;
+})
+let coinCount = 100;
+document.getElementById('call-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    // const serviceName = {
+    //     name: document.getElementById('service-name')
+    // };
+
+    if (coinCount >= 20) {
+        coinCount -= 20;
+        alert('call button clicked');
+        const countCoinValue = document.getElementById('coin-number');
+        countCoinValue.textContent = coinCount;
+
+    } else {
+        alert('Insufficient coin');
+    }
+})
